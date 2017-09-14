345186a4a22df808a435b33a22407e8c361f667a
annabush092 <annabush092@gmail.com>
2017-09-14 13:39:00 -0400

    Passed all tests but guest trip association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,19 @@
-class User < ActiveRecord::Base[4.2]
+class User < ActiveRecord::Base
 
   has_many :listings, :foreign_key => 'host_id'
+  has_many :reservations, :foreign_key => 'guest_id'
+  has_many :reviews, :foreign_key => "guest_id"
+  has_many :reservations, :through => :listings, :foreign_key => "host_id"
+
+  has_many :trips, :class_name => "reservations", :foreign_key => 'guest_id'
 
 end
+
+# has_many
+# belongs_to
+#
+# has_many :blah, :through => blah
+#
+# belongs_to :blah, :class_name => "blah"
+# has_many :blah, :foreign_key => "guest_id"
+# "guest has many blah"
