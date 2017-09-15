edd7437d10cf82cbb823874667cc37b1d3675514
github username <github email address>
2017-09-14 20:10:05 -0400

    Done.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,9 +1,9 @@
 class Reservation < ActiveRecord::Base
   belongs_to :listing
   has_many :reviews
-  
-  # old version: Didn't work.
-  # belongs_to :guest, :class_name => "User"
-  belongs_to :guest, :class_name => "User", :foreign_key => "guest_id"
 
+  # old version: Didn't work.
+  belongs_to :guest, :class_name => "User"
+
+  # belongs_to :guest, :class_name => "User", :foreign_key => "guest_id"
 end
